5262112bcef9a1dbe0a13be9c747fab4435fc35e
elshod1018 <nuriddinovelshod2003@gmail.com>
2023-05-09 22:15:44 +0500

    subjects

---
+++ target/generated-sources/annotations/uz/test/abitur/mapper/QuestionMapperImpl.java
@@ -0,0 +1,28 @@
+package uz.test.abitur.mapper;
+
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+import uz.test.abitur.domains.Question;
+import uz.test.abitur.dtos.question.QuestionCreateDTO;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-05-09T22:14:58+0500",
+    comments = "version: 1.5.3.Final, compiler: javac, environment: Java 17.0.6 (Private Build)"
+)
+@Component
+public class QuestionMapperImpl implements QuestionMapper {
+
+    @Override
+    public Question toEntity(QuestionCreateDTO dto) {
+        if ( dto == null ) {
+            return null;
+        }
+
+        Question.QuestionBuilder question = Question.childBuilder();
+
+        question.text( dto.getText() );
+
+        return question.build();
+    }
+}

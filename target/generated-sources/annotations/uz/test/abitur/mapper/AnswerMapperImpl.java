677b5514d5ccfafffbd31ada45a16af0367e8cf1
elshod1018 <nuriddinovelshod2003@gmail.com>
2023-05-09 22:14:15 +0500

    subjects

---
+++ target/generated-sources/annotations/uz/test/abitur/mapper/AnswerMapperImpl.java
@@ -0,0 +1,28 @@
+package uz.test.abitur.mapper;
+
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+import uz.test.abitur.domains.Answer;
+import uz.test.abitur.dtos.question.AnswerCreateDTO;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-05-09T19:48:43+0500",
+    comments = "version: 1.5.3.Final, compiler: javac, environment: Java 17.0.6 (Private Build)"
+)
+@Component
+public class AnswerMapperImpl implements AnswerMapper {
+
+    @Override
+    public Answer toEntity(AnswerCreateDTO dto) {
+        if ( dto == null ) {
+            return null;
+        }
+
+        Answer.AnswerBuilder answer = Answer.builder();
+
+        answer.text( dto.getText() );
+
+        return answer.build();
+    }
+}
